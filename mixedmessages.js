dc5e6eb5ad97d37826b4120b55c299e3eab0ebbd
quickslidesam <sam.jones@quickslide.co.uk>
2025-02-28 11:23:31 +0000

    add functionality

--- mixedmessages.js
+++ mixedmessages.js
@@ -10,4 +10,4 @@ function randomNumber(arr) {
     return Math.floor(Math.random() * arr.length)
 };
 
-console.log(createMessage());
+document.getElementById('message').textContent = createMessage();

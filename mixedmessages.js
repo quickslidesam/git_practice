f1ffdff20881352dac1a1b1a357696c45b46ca9e
quickslidesam <sam.jones@quickslide.co.uk>
2025-02-28 11:16:32 +0000

    First commit

---
+++ mixedmessages.js
@@ -0,0 +1,13 @@
+const subject = ['You', 'We', 'He', 'She', 'They'];
+const verb = ['can', "can't", "will", "won't", "must", "musn't"];
+const action = ['jump off a cliff', 'bake bread', "break bad", "eat beans"];
+
+function createMessage() {
+    return `${subject[randomNumber(subject)]} ${verb[randomNumber(verb)]} ${action[randomNumber(action)]}!`
+};
+
+function randomNumber(arr) {
+    return Math.floor(Math.random() * arr.length)
+};
+
+console.log(createMessage());
